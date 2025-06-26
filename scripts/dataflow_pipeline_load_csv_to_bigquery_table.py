da79db9a7ad971c388aa7e4d1fb629ec7a637c3d
sabithamallela <sabitha.mallela@gmail.com>
2025-06-26 19:39:30 +0100

    updated dataflow pipeline with parse and validation functions

--- scripts/dataflow_pipeline_load_csv_to_bigquery_table.py
+++ scripts/dataflow_pipeline_load_csv_to_bigquery_table.py
@@ -1,6 +1,32 @@
 import argparse, apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from transforms import parse_and_validate_customers, parse_and_validate_transactions
+import csv
+
+def parse_and_validate_customers(line):
+    try:
+        row = next(csv.reader([line]))
+        return {'customer_id': row[0],
+                'first_name': row[1],
+                'last_name': row[2],
+                'email': row[3],
+                'signup_date': row[4]
+                }
+    except Exception as e:
+        print(f"Parse error: {e}")
+        return None
+
+def parse_and_validate_transactions(line):
+    try:
+        row = next(csv.reader([line]))  # parse line into a list
+        return {
+            'transaction_id': row[0],
+            'customer_id': row[1],
+            'transaction_amount': float(row[2]),
+            'transaction_time': row[3]
+        }
+    except Exception as e:
+        print(f"Parse error: {e}")
+        return None
 
 def run(argv=None):
     parser = argparse.ArgumentParser()
@@ -26,11 +52,11 @@ def run(argv=None):
             schema = 'transaction_id:STRING,customer_id:STRING,transaction_amount:FLOAT,transaction_time:TIMESTAMP'
 
         data | 'FilterValid' >> beam.Filter(lambda r: r) \
-             | 'WriteBQ' >> beam.io.WriteToBigQuery(
-                 args.output_table, schema=schema,
-                 create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-                 write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
-             )
+        | 'WriteBQ' >> beam.io.WriteToBigQuery(
+            args.output_table, schema=schema,
+            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
+        )
 
 if __name__ == '__main__':
     run()
