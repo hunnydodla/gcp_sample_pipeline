87b28de7921b13714440bfa4c9672e7c21944fec
sabithamallela <sabitha.mallela@gmail.com>
2025-06-27 02:35:53 +0100

    removed absolute path, as this is not required

--- scripts/dataflow_ingest_csv_to_bq_main.py
+++ scripts/dataflow_ingest_csv_to_bq_main.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 import argparse
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
