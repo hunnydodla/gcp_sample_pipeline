639ea573b96d3b43ae618ac5f5684b344b8652e5
sabithamallela <sabitha.mallela@gmail.com>
2025-07-02 01:26:59 +0100

    dummy update to run lint and tests1

--- Run_scripts/run_scripts.sh
+++ Run_scripts/run_scripts.sh
@@ -1,4 +1,4 @@
-# Run below scripts in cloud shell,
+# Run below scripts in cloud shell manually,
 
 python3 dataflow_ingest_csv_to_bq_main.py \
  --input gs://customer_analytics-bucket/data/transactions.csv \
